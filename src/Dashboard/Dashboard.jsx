8b2bb17c86920aeb4f40300fa8cc413a69b97237
Randy Odoom <randyodoom19@gmail.com>
2022-11-27 23:40:03 +0000

    changes

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ function Dashboard() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (userInfo.length == 0) {
+    if (userInfo?.length == 0) {
       navigate("/");
     }
   }, []);
