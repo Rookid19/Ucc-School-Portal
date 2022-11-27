625104f6b481e9b7de7d86359b6caa29c2e1ff54
Randy Odoom <63527175+Rookid19@users.noreply.github.com>
2022-11-27 22:23:54 +0000

    Update Homepage.jsx

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -14,63 +14,7 @@ function Homepage() {
   const [loading, setLoading] = useState("Submit");
 
   // [A-Z]{2}/[A-Z]{3}/[0-9]{2}/[0-9]{4}
-  // const setUpRecaptcha = () => {
-  //   window.recaptchaVerifier = new RecaptchaVerifier(
-  //     "recaptcha-container",
-  //     {
-  //       size: "invisible",
-  //       callback: (response) => {
-  //         // reCAPTCHA solved, allow signInWithPhoneNumber.
-  //         console.log("recaptach resolved");
-  //         onSignInSubmit();
-  //       },
-  //     },
-  //     auth
-  //   );
-  // };
 
-  // const onSignInSubmit = (event) => {
-  //   event.preventDefault();
-  //   setUpRecaptcha();
-  //   const phoneNumber = "+233240727345";
-  //   const appVerifier = window.recaptchaVerifier;
-
-  //   signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-  //     .then((confirmationResult) => {
-  //       // SMS sent. Prompt user to type the code from the message, then sign the
-  //       // user in with confirmationResult.confirm(code).
-  //       window.confirmationResult = confirmationResult;
-  //       console.log(
-  //         "confirmationResult ----> " + JSON.stringify(confirmationResult)
-  //       );
-  //       // ...
-  //     })
-  //     .catch((error) => {
-  //       alert(error);
-  //       console.log("Invalid Otp verification code");
-  //       // Error; SMS not sent
-  //       // ...
-  //     });
-  // };
-
-  // const verifyOtp = (e) => {
-  //   e.preventDefault();
-  //   const code = "520673";
-  //   let confirmationResult = window.confirmationResult;
-  //   confirmationResult
-  //     .confirm(code)
-  //     .then((result) => {
-  //       // User signed in successfully.
-  //       const user = result.user;
-  //       // ...
-  //       alert("Successully signed in");
-  //     })
-  //     .catch((error) => {
-  //       alert(error);
-  //       // User couldn't sign in (bad verification code?)
-  //       // ...
-  //     });
-  // };
 
   return (
     <main id="main" className="login-body">
@@ -80,7 +24,7 @@ function Homepage() {
             src="https://portal.ucc.edu.gh/reset/assets/images/ucc_logo.png"
             className="img-fluid"
           />
-          <h3>Online Students' Portal </h3>
+          <h3>Online Students' Portal randy</h3>
         </div>
       </Fade>
       <div className="login-container">
